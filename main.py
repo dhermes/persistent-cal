a32bc841fda94fcde879ba7a085c50a0a4f4e6b0
Daniel Hermes <dhermes@google.com>
2011-07-21 15:51:26 -0700

    Testing local git config

---
+++ main.py
@@ -0,0 +1,34 @@
+import os
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+
+class MainHandler(webapp.RequestHandler):
+  """Handles / as well as redirects for login required"""
+  def get(self):
+    path = os.path.join(os.path.dirname(__file__), 'templates', 'index.html')
+    self.response.out.write(template.render(path, {}))
+
+
+class Throw404(webapp.RequestHandler):
+  """Catches all non-specified (404) requests"""
+  def get(self):
+    self.error(404)
+    path = os.path.join(os.path.dirname(__file__), 'templates', '404.html')
+    self.response.out.write(template.render(path, {}))
+
+
+application = webapp.WSGIApplication([
+  ('/', MainHandler),
+  ('/.*', Throw404),
+  ], debug=True)
+
+
+def main():
+    run_wsgi_app(application)
+
+
+if __name__ == '__main__':
+    main()
