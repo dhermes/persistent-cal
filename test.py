1f3045c0369a48762d952e62ab6efa2749fc7027
Daniel Hermes <dhermes@google.com>
2011-07-26 14:20:08 -0700

    Added data structure to test.py (tested in main), updated TODO. Will try to incorporate header 'last-updated' but TripIt does not track this correctly

--- test.py
+++ test.py
@@ -1,59 +1,64 @@
 from icalendar import Calendar
+import pickle
 from urllib2 import urlopen
 from urllib2 import urlparse
+
+def pickle_event(event):
+  # all events have a UID, and almost all begin with 'item-';
+  # those that don't are an away event for the entire trip
+  uid = unicode(event.get('uid'))
+  to_pickle = {'when:from': event.get('dtstart').dt,
+               'when:to': event.get('dtend').dt,
+               'summary': unicode(event.get('summary')),
+               'location': unicode(event.get('location')),
+               'description': unicode(event.get('description'))}
+  return uid, pickle.dumps(to_pickle)
 
 link = ('webcal://www.tripit.com/feed/ical/private/'
         '3F43994D-4591D1AA4C63B1472D8D5D0E9568E5A8/tripit.ics')
 link = 'http:%s ' % urlparse.urlparse(link).path
 
 feed = urlopen(link)
+request_headers = feed.headers.dict
+last_modified = request_headers['last-modified'] # trip it doesn't respect this
 cal_feed = feed.read()
 feed.close()
 
 ical = Calendar.from_string(cal_feed)
-name = ical.get('X-WR-CALNAME').__str__()
+name = unicode(ical.get('X-WR-CALNAME'))
 for component in ical.walk():
   if component.name == "VEVENT":
-    print component.get('summary')
-    print component.get('dtstart')
-    print component.get('dtend')
-    print component.get('dtstamp')
-
-# VEVENT(
-#   {'DTSTAMP': <icalendar.prop.vDDDTypes instance at 0x10152aab8>,
-#    'UID': vText(u'item-a218099f87f84bb18818b575b345d9333d4260aa@tripit.com'),
-#    'SUMMARY': vText(u'WN1999 LAS to SFO'),
-#    'LOCATION': vText(u'Las Vegas (LAS)'),
-#    'DTEND': <icalendar.prop.vDDDTypes instance at 0x10152ab00>,
-#    'DTSTART': <icalendar.prop.vDDDTypes instance at 0x10152ab48>,
-#    'GEO': <icalendar.prop.vGeo instance at 0x10152ab90>,
-#    'DESCRIPTION': vText(u'View and/or edit details in TripIt : '
-#                         'http://www.tripit.com/trip/show/id/18587799\n \n'
-#                         '[Flight] 10/1/2011 Southwest Airlines(WN) #1999 dep '
-#                         'LAS 6:05pm PDT arr SFO 7:35pm PDT; Daniel Jerome '
-#                         'Hermes, Sharona Haya Franko; conf #W95N92 \nBooked '
-#                         'on http://www.southwest.com/; '
-#                         'http://www.southwest.com/; 1-800-435-9792 \n \n \n\n'
-#                         'TripIt - organize your travel at http://www.tripit.com'
-#                         )})
-
-
-# TODO: incorporate default reminders (or ask),
-#       daylight savings
-
-# component.get('dtend').dt = datetime.datetime object
+    uid, pickle_contents = pickle_event(component)
+    print uid
 
 # GCal -> iCal correspondences:
 # - title -> summary
-# - when:from -> dstart
-# - when:to -> dend
+# - when:from -> dtstart
+# - when:to -> dtend
 # - where -> location
 # - calendar -> None
 # - description -> description
 # - reminders -> None
 # Note: geo gives but who knows what for lat and lng
 
-ff.name == 'VCALENDAR'
-title = ff.get('X-WR-CALNAME').__str__()
 
-# component.uid should be no repeat - vText(u'item-a218099f87f84bb18818b575b345d9333d4260aa@tripit.com')
+# Event model
+# from google.appengine.ext import db
+
+
+# class Event(db.Model):
+#   """Holds data for a calendar event (including shared owners)"""
+#   owners = db.StringListProperty(required=True) # hold owner id's as strings
+#   event_data = db.TextProperty(required=True) # will be python dict in pickle
+
+# e = Event(key_name='item-a218099f87f84bb18818b575b345d9333d4260aa@tripit.com',
+#           owners=['a','b','c'],
+#           event_data = 'a')
+# e.put()
+#     e = Event.all().get()
+#     e.owners.append('a')
+#     e.put()
+
+
+# use
+# Event(key_name=uid, ...)
